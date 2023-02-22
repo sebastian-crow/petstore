32854921db17093bf1ac95ba0f922d71a44d4adf
sebastian-crow <sebastianberiio@gmail.com>
2023-02-22 02:01:04 -0500

    Giving up pet form

---
+++ Pages/Entregar/main.js
@@ -0,0 +1,38 @@
+const { createApp, ref } = Vue;
+
+createApp({
+  data() {
+    return {
+      pets: "",
+      User: "",
+      AdminUser: "",
+      showModal: ref(false),
+      userPets: [],
+    };
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem("User");
+      localStorage.removeItem("AdminUser");
+      location.href = "../../index.html";
+    },
+  },
+  beforeMount() {
+    this.User = JSON.parse(localStorage.getItem("User"));
+    this.AdminUser = JSON.parse(localStorage.getItem("AdminUser"));
+    this.pets = JSON.parse(localStorage.getItem("pets"));
+  },
+  mounted() {
+    const ownPets = JSON.parse(JSON.stringify(this.pets));
+    this.userPets = ownPets.filter((p) => {
+      if (p.owner?.id === this.User?.login?.uuid) {
+        return p;
+      }
+    });
+    console.log(this.userPets);
+    console.log(this.pets);
+  },
+  updated() {},
+})
+  .use(naive)
+  .mount("#entregar");
